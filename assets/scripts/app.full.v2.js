5de0f9dabdf4e046ef412f7d867235ac2547486d
Vasilisa <emmchik@mail.ru>
2023-03-07 20:33:06 +1300

    added edit remove buttons working on functions related

--- assets/scripts/app.full.v2.js
+++ assets/scripts/app.full.v2.js
@@ -5,8 +5,14 @@ const authorInput = document.getElementById("create-author");
 const genreInput = document.getElementById("create-genre");
 const priceInput = document.getElementById("create-price");
 const ownerInput = document.getElementById("create-owner");
+const previewSection = document.getElementById("preview-section");
 const booksContainer = document.getElementById("books-container");
+const createImageInput = document.getElementById("create-image");
 
+// Function to generate a unique ID for a new book
+function generateId() {
+    return Math.random().toString(36).substring(2) + Date.now().toString(36);
+}
 
 // Load books from local storage
 let books = localStorage.getItem("books");
@@ -33,37 +39,83 @@ createForm.addEventListener("submit", function (event) {
         authorInput.value,
         genreInput.value,
         parseFloat(priceInput.value),
-        ownerInput.value
+        ownerInput.value,
+        createImageInput.files[0]
     );
     createForm.reset();
 });
+// Add event listener to create image input
+createImageInput.addEventListener("change", function (event) {
+    const previewSection = document.getElementById("preview-section");
+    const previewImage = document.createElement("img");
+    previewImage.setAttribute("height", "200");
+    previewImage.setAttribute("src", URL.createObjectURL(event.target.files[0]));
+    previewSection.appendChild(previewImage);
+});
 
 // Function to create a new book
-function createBook(title, author, genre, price, owner) {
+function createBook(title, author, genre, price, owner, image) {
+
     // Create a new book object
     const newBook = {
+        id: generateId(),
         title: title,
         author: author,
         genre: genre,
         price: price,
         owner: owner,
-        rating: 1 // Set initial rating to 0
+        rating: 1, // Set initial rating to 1
+        image: image ? URL.createObjectURL(image) : null // Set image URL if image is uploaded, otherwise set to null
     };
 
+    // Create the edit and remove buttons with the required attributes
+    const editBtn = document.createElement("button");
+    editBtn.setAttribute("class", "btn edit-btn");
+    editBtn.textContent = "Edit";
+    const removeBtn = document.createElement("button");
+    removeBtn.setAttribute("class", "btn remove-btn");
+    removeBtn.textContent = "Remove";
+
+    // Add event listeners to the edit and remove buttons
+    editBtn.addEventListener("click", () => {
+        editBook(newBook.id);
+    });
+    removeBtn.addEventListener("click", () => {
+        removeBook(newBook.id);
+    });
+
+    // Store the image URL in local storage
+    if (image) {
+        const reader = new FileReader();
+        reader.readAsDataURL(image);
+        reader.onload = function () {
+            const imageURL = reader.result;
+            newBook.image = imageURL; // Set the image URL in the newBook object
+            localStorage.setItem(`book-image-${newBook.id}`, imageURL); // Set the key for the image URL in local storage
+            console.log(localStorage); // Log the localStorage object to the console
+        };
+    }
+    
     // Add the new book to the books array
     books.push(newBook);
 
-    // Store the updated books array in local storage
-    localStorage.setItem("books", JSON.stringify(books));
+    // Update local storage with the new book object
+  localStorage.setItem("books", JSON.stringify(books));
+
+
+
+    // Reset image preview
+    previewSection.innerHTML = "";
 
     // Create a new section element with the required attributes
     const newSection = document.createElement("section");
-        
+    newSection.setAttribute("data-book-id", newBook.id);
+    
     // Create the image element with the required attributes
-    const image = document.createElement("img");
-    image.setAttribute("src", "./assets/images/book.jpg");
-    image.setAttribute("height", "330");
-    newSection.appendChild(image);
+    const bookImage = document.createElement("img");
+    bookImage.setAttribute("height", "330");
+    bookImage.setAttribute("src", newBook.image || "./assets/images/book.jpg"); // Set the src attribute to the image URL or default image
+    newSection.appendChild(bookImage);
     newSection.appendChild(document.createElement("br"));
 
     // Create the title and price elements with the required attributes
@@ -153,6 +205,9 @@ function createBook(title, author, genre, price, owner) {
     detailsBtn.setAttribute("class", "btn");
     detailsBtn.textContent = "Book Details";
     newSection.appendChild(detailsBtn);
+    // Append the edit and remove buttons to the section
+    newSection.appendChild(editBtn);
+    newSection.appendChild(removeBtn);
 
     // Append the new section to the books container
     booksContainer.prepend(newSection);
@@ -160,4 +215,108 @@ function createBook(title, author, genre, price, owner) {
 
 }
 
+function renderBook(book) {
+    // Find the book section on the page
+    const bookSection = document.querySelector(`[data-book-id="${book.id}"]`);
+    if (!bookSection) {
+        console.error(`Book section not found for book ID ${book.id}`);
+        return;
+    }
+}
 
+// Function to edit a book
+function editBook(bookId) {
+    // Find the book in the books array by its ID
+    const bookIndex = books.findIndex(book => book.id === bookId);
+    const book = books[bookIndex];
+
+    // Find the book section on the page
+    const bookSection = document.querySelector(`[data-book-id="${bookId}"]`);
+    if (!bookSection) {
+        console.error(`Book section not found for book ID ${bookId}`);
+        return;
+    }
+    // Create an edit form with input fields for each book property
+    const editForm = document.createElement("form");
+    const titleInput = document.createElement("input");
+    titleInput.setAttribute("type", "text");
+    titleInput.setAttribute("placeholder", "Title");
+    titleInput.value = book.title;
+    const authorInput = document.createElement("input");
+    authorInput.setAttribute("type", "text");
+    authorInput.setAttribute("placeholder", "Author");
+    authorInput.value = book.author;
+    const genreInput = document.createElement("input");
+    genreInput.setAttribute("type", "text");
+    genreInput.setAttribute("placeholder", "Genre");
+    genreInput.value = book.genre;
+    const priceInput = document.createElement("input");
+    priceInput.setAttribute("type", "number");
+    priceInput.setAttribute("placeholder", "Price");
+    priceInput.value = book.price;
+    const ownerInput = document.createElement("input");
+    ownerInput.setAttribute("type", "text");
+    ownerInput.setAttribute("placeholder", "Owner");
+    ownerInput.value = book.owner;
+    const imageInput = document.createElement("input");
+    imageInput.setAttribute("type", "file");
+    const submitBtn = document.createElement("button");
+    submitBtn.setAttribute("type", "submit");
+    submitBtn.textContent = "Save Changes";
+
+    // Add an event listener to the edit form
+    editForm.addEventListener("submit", (event) => {
+        event.preventDefault();
+
+        // Update the book object with the new property values
+        book.title = titleInput.value;
+        book.author = authorInput.value;
+        book.genre = genreInput.value;
+        book.price = parseFloat(priceInput.value);
+        book.owner = ownerInput.value;
+
+        if (imageInput.files.length > 0) {
+            const reader = new FileReader();
+            reader.readAsDataURL(imageInput.files[0]);
+            reader.onload = function () {
+                const imageURL = reader.result;
+                book.image = imageURL; // Set the image URL in the book object
+                localStorage.setItem(`book-image-${book.id}`, imageURL); // Set the key for the image URL in local storage
+                console.log(localStorage); // Log the localStorage object to the console
+            };
+        }
+
+        // Update the book in the books array and local storage
+        books[bookIndex] = book;
+        localStorage.setItem("books", JSON.stringify(books));
+
+        // Render the updated book on the page
+        renderBook(book);
+    });
+
+     // Append the input fields and submit button to the edit form
+    editForm.appendChild(titleInput);
+    editForm.appendChild(authorInput);
+    editForm.appendChild(genreInput);
+    editForm.appendChild(priceInput);
+    editForm.appendChild(ownerInput);
+    editForm.appendChild(imageInput);
+    editForm.appendChild(submitBtn);
+
+    // Replace the book section with the edit form
+    bookSection.replaceWith(editForm);
+}
+
+// Function to remove a book
+function removeBook(bookId) {
+    // Find the book in the books array by its ID
+    const bookIndex = books.findIndex(book => book.id === bookId);
+
+    // Remove the book from the books array and local storage
+    books.splice(bookIndex, 1);
+    localStorage.setItem("books", JSON.stringify(books));
+
+    // Remove the book section from the page
+    const bookSection = document.querySelector(`[data-book-id="${bookId}"]`);
+    bookSection.remove();
+}
--- assets/scripts/navi.login.js
+++ assets/scripts/navi.login.js
@@ -10,3 +10,16 @@ responsemenu.addEventListener("click", () => {
 close.addEventListener("click", () => {
     menu.classList.remove("show");
 });
+
+// ==== Login Form ===== //
+// Select header wrapper
+const loginForm = document.querySelector("header .wrapper");
+
+// Onclick add active class
+document.querySelector(".login").onclick = () => {
+    loginForm.classList.add("active");
+};
+//Onclick remove active class
+document.querySelector(".close-form").onclick = () => {
+    loginForm.classList.remove("active");
+};
